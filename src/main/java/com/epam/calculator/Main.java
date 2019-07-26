98799feb543b15a2fe6631c12ff75a77a4126800
Oleg Timofeev <oleg_timofeev@epam.com>
2019-07-26 14:17:14 +0300

    Fix regexp

--- src/main/java/com/epam/calculator/Main.java
+++ src/main/java/com/epam/calculator/Main.java
@@ -3,13 +3,13 @@ package com.epam.calculator;
 import java.util.Scanner;
 
 public class Main {
-    public static final String regExp= "-?(sqrt\\d+|\\d+) (([*/+\\-]) -?(sqrt\\d+|\\d+|-\\d+) )+";
+    public static final String regExp= "-?(sqrt\\d+|\\d+|\\d+.\\d+) (([*/+\\-]) -?(sqrt\\d+|\\d+|-\\d+|\\d+.\\d+) )+";
 
     public static void main(String[] args){
         QueueCalculator qc = new QueueCalculator();
         Scanner sc = new Scanner(System.in);
         String str;
-        System.out.println("Для операции sqrt число пишется сразу после ( Пример sqrt9 ) \nЕсли число отрицательное, то слитно( Пример -7 )\n В других случаях цифры и знаки пишутся через пробел ");
+        System.out.println("Для операции sqrt число пишется сразу после ( Пример sqrt9 ) \nЕсли число отрицательное, то слитно( Пример -7 )\nВ других случаях цифры и знаки пишутся через пробел ");
         System.out.println("Введите пример");
         while(true) {
             str = sc.nextLine().trim()+" ";
